e5f5469161377a816779ecb7910b368afc09d1f9
kevohm <kevinkbet@gmail.com>
2022-09-12 04:19:28 -0700

    done

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,4 @@
+CREATE USER IF NOT EXISTS "hbnb_dev"@"localhost" IDENTIFIED BY "hbnb_dev_pwd";
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost' WITH GRANT OPTION;
+GRANT SELECT on performance_schema.* TO 'hbnb_dev'@'localhost' WITH GRANT OPTION;
