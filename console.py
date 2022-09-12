3690f310590c802e1fa485fd9fd970d0f1a66c0b
kevohm <kevinkbet@gmail.com>
2022-09-12 12:45:51 -0700

    done

--- console.py
+++ console.py
@@ -115,30 +115,38 @@ class HBNBCommand(cmd.Cmd):
         pass
 
     def do_create(self, args):
-        """ Create an object of any class"""
-        data = split(args)
-        print(data)
-        if not args:
+        """Usage: create <class> <key 1>=<value 2> <key 2>=<value 2> ...
+        Create a new class instance with given keys/values and print its id.
+        """
+        try:
+            if not line:
+                raise SyntaxError()
+            my_list = line.split(" ")
+
+            kwargs = {}
+            for i in range(1, len(my_list)):
+                key, value = tuple(my_list[i].split("="))
+                if value[0] == '"':
+                    value = value.strip('"').replace("_", " ")
+                else:
+                    try:
+                        value = eval(value)
+                    except (SyntaxError, NameError):
+                        continue
+                kwargs[key] = value
+
+            if kwargs == {}:
+                obj = eval(my_list[0])()
+            else:
+                obj = eval(my_list[0])(**kwargs)
+                storage.new(obj)
+            print(obj.id)
+            obj.save()
+
+        except SyntaxError:
             print("** class name missing **")
-            return
-        elif data[0] not in HBNBCommand.classes:
+        except NameError:
             print("** class doesn't exist **")
-            return
-        new_instance = HBNBCommand.classes[data[0]]()
-        for i in range(1, len(data)):
-            if "=" in data[i]:
-                key, value = tuple(data[i].split("="))
-            if value[0] == '"':
-                value = value.strip('"')
-            value = value.replace("_", " ")
-            try:
-                value = eval(value)
-            except BaseException:
-                continue
-            if value != "":
-                setattr(new_instance, key, value)
-        storage.save()
-        print(new_instance.id)
 
     def help_create(self):
         """ Help information for the create method """
