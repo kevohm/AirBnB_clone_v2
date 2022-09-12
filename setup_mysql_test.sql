4162c1bf38c196087e641d52c60c200e19191ade
Kevinkibet <58782210+kevohm@users.noreply.github.com>
2022-09-12 14:39:36 +0300

    Update setup_mysql_test.sql

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,2 +1,6 @@
---test database exist
-USE hbnb_test_db;
+-- test create user and database
+-- grant test user all privilages to test db created
+CREATE USER IF NOT EXISTS "hbnb_test"@"localhost" IDENTIFIED BY "hbnb_test_pwd";
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost' WITH GRANT OPTION;
+GRANT SELECT on performance_schema.* TO 'hbnb_test'@'localhost' WITH GRANT OPTION;
