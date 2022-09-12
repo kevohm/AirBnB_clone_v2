72dfb03bc8484ff5043e5b39f02ed8c185186bcb
kevohm <kevinkbet@gmail.com>
2022-09-12 04:23:33 -0700

    commented

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,3 +1,5 @@
+-- create user and database
+-- grant user all privilages to db created
 CREATE USER IF NOT EXISTS "hbnb_dev"@"localhost" IDENTIFIED BY "hbnb_dev_pwd";
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost' WITH GRANT OPTION;
