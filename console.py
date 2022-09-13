f0bbac8f0df69e46d0e2337befe5c2993aee83f3
kevohm <kevinkbet@gmail.com>
2022-09-13 06:21:48 -0700

    done

--- console.py
+++ console.py
@@ -116,28 +116,32 @@ class HBNBCommand(cmd.Cmd):
 
     def do_create(self, line):
         """Usage: create <class> <key 1>=<value 2> <key 2>=<value 2> ...
-        Create a new class instance with given keys/values and print its id.
-        """
+        Create a new class instance with given keys/values and print its id."""
         try:
             if not line:
                 raise SyntaxError()
             my_list = split(line)
-            obj = eval(my_list[0])()
-            for attr in my_list[1:]:
-                if "=" not in attr:
-                    continue
-                key, value = tuple(attr.split('='))
+            kwargs = {}
+            for i in range(1, len(my_list)):
+                key, value = tuple(my_list[i].split("="))
+                print(value)
                 try:
-                    value = int(value)
-                except ValueError:
+                    value = value.strip('"').replace("_", " ")
+                except Exception:
                     try:
-                        value = float(value)
-                    except ValueError:
-                        value = value.strip('\"').replace("_", " ")
-                setattr(obj, key, value)
-                print(obj)
+                        value = eval(value)
+                    except (SyntaxError, NameError):
+                        continue
+                kwargs[key] = value
+
+            if kwargs == {}:
+                obj = eval(my_list[0])()
+            else:
+                obj = eval(my_list[0])(**kwargs)
+                storage.new(obj)
+            print(obj.id)
             obj.save()
-            print("{}".format(obj.id))
+
         except SyntaxError:
             print("** class name missing **")
         except NameError:
--- models/base_model.py
+++ models/base_model.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """This module defines a base class for all models in our hbnb clone"""
-import uuid, models
+from uuid import uuid4
+import models
 from datetime import datetime
 from sqlalchemy import Column, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
@@ -14,16 +15,14 @@ class BaseModel():
     updated_at = Column(DateTime, default=datetime.utcnow(), nullable=False)
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
-        if not kwargs:
-            self.id = str(uuid.uuid4())
-            self.created_at = self.updated_at = datetime.utcnow()
-        else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            del kwargs['__class__']
-            self.__dict__.update(kwargs)
+        self.id = str(uuid4())
+        self.created_at = self.updated_at = datetime.utcnow()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                if key != "__class__":
+                    setattr(self, key, value)
 
     def __str__(self):
         """Returns a string representation of the instance"""
