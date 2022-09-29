6dc4ff9fb8b57658e7fbf2e5fcb12a02d5b4e114
kevohm <kevinkbet@gmail.com>
2022-09-29 08:01:35 -0700

    fixed

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -7,7 +7,7 @@ from fabric.api import local
 from fabric.api import put
 from fabric.api import run
 
-env.hosts = ["104.196.168.90", "35.196.46.172"]
+env.hosts = ["3.226.254.64", "3.238.244.16"]
 
 
 def do_pack():
