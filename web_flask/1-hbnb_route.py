5978ad71d87f30fa00dedb249efbd74b05565900
Kevinkibet <58782210+kevohm@users.noreply.github.com>
2022-10-18 17:02:38 +0300

    Update 1-hbnb_route.py

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,18 +1,24 @@
 #!/usr/bin/python3
+"""
+A simple flask server running on 0.0.0.0:5000
+"""
 from flask import Flask
 
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
-@app.route("/")
-def hello_world():
+@app.route('/')
+def display_root():
+    """Prints 'Hello HBNB!' to display"""
     return "Hello HBNB!"
 
 
-@app.route("/hbnb")
-def hello():
+@app.route('/hbnb')
+def display_hbnb():
+    """Prints 'HBNB' to display"""
     return "HBNB"
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
