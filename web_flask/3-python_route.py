740bd9fe3fd51ea89881ee293501909eaddfff54
Kevinkibet <58782210+kevohm@users.noreply.github.com>
2022-10-18 17:17:22 +0300

    Update 3-python_route.py

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,1 +1,39 @@
-#!
+#!/usr/bin/python3
+"""
+A simple flask server running on 0.0.0.0:5000
+"""
+from flask import Flask
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def display_root():
+    """Prints 'Hello HBNB!' to display"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb')
+def display_hbnb():
+    """Prints 'HBNB' to display"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def display_c(text):
+    """Displays 'C' followed by the value of <text>."""
+    text = text.replace("_", " ")
+    return "C {}".format(text)
+
+
+@app.route('/python/')
+@app.route('/python/<text>')
+def display_python(text="is cool"):
+    """Print route param to display"""
+    text = text.replace("_", " ")
+    return "Python {}".format(text)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
